e0a3c927a1efe3676286d8bdf2a40eabfb23c900
bohdanuhryn <bohdanuhryn@gmail.com>
2017-08-12 22:34:58 +0300

    update

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = t('html_head_title')
+  config.site_title = "Uhrynovskyi Bohdan - personal page"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
